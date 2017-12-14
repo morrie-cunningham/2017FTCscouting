25195f68675c0246b77f6b6cbf5b75c7cc6eb5c6
Morrie Cunningham <cunningm@mail.gvsu.edu>
2017-12-13 23:32:48 -0500

    Added State teams.

--- app/src/main/java/com/example/morri/ftc_scouting_2017/MainActivity.java
+++ app/src/main/java/com/example/morri/ftc_scouting_2017/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         spinnerDT.setAdapter(DTadapt);
 
         Spinner spinnerTeams = (Spinner) findViewById(R.id.teamNumberSpinner);
-        ArrayAdapter<CharSequence> TeamsAdapt = ArrayAdapter.createFromResource(this, R.array.BigRapidsQualifierTeams, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> TeamsAdapt = ArrayAdapter.createFromResource(this, R.array.StateTeams, android.R.layout.simple_spinner_item);
         TeamsAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerTeams.setAdapter(TeamsAdapt);
 
